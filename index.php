38de2cad1bda2e64cb7413c64cd821814786ba50
Aline Mokfa <alinemokfa@hotmail.co.uk>
2018-02-09 22:15:53 +0000

    setup files

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Hello, world!</title>
+</head>
+<body>
+    <h1>Title</h1>
+    <?php 
+
+    $name = 'Aline';
+
+    echo "Hello, $name!";
+
+    ?>
+</body>
+</html>
